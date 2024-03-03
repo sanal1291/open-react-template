b59654527ae0b86aca04320a63fe1d3e776ac020
sanal1291 <54595945+sanal1291@users.noreply.github.com>
2024-03-03 20:41:25 +0530

    first deploy

--- components/features.tsx
+++ components/features.tsx
@@ -6,7 +6,7 @@ export default function Features() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            {/* <h2 className="h2 mb-4">The majority our customers do not understand their workflows.</h2> */}
+            <h2 className="h2 mb-4">The majority our customers do not understand their workflows.</h2>
             <p className="text-xl text-gray-400">TRUSTON was established in 2017 as a partnership firm to serve the quality Polymers & customized products to meet the thrust of engineering growth in India. THREE decades of experience & technical expertise in the Polymer processing industry helps us to identify perfect material combination for each application.</p>
           </div>
 
